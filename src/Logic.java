436fa51b9f23d0defbab3cb4cbdc9d415143280f
Farzin Riham <friham07@Gmail.com>
2023-12-03 12:07:51 -0500

    normal mode finally updated with string methods and element select 12:07pm December 3, 2023

--- src/Logic.java
+++ src/Logic.java
@@ -27,14 +27,35 @@ public class Logic {
                 scan.nextLine();
                 userConfirm = scan.nextLine();
             }
+            System.out.println("Lastly, there are two elements to pick from: Light, and Dark.");
+            System.out.print("Press 1 to select Light, or 2 to select Dark (this will not affect your power or give you any quirks, it's just customization) ");
+            int input = scan.nextInt();
+            game.elementSelect("Light, Dark", input);
+            System.out.println("Your element has been set to " + game.getElement() + ".");
             System.out.println("Great! Starting game....");
-            System.out.println("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~");
+            System.out.println("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~* New Game *~*~*~~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~");
             // game logic starts here
+            if (game.getElement().equals("Light")) {
+                System.out.println("Rebellious beasts from the Dark Realm roam your hometown, terrorizing your family, friends, and people.");
+                System.out.println("The Light Nation needs the help of an outstanding, brave citizen to yield the sword of Sun and eradicate these foul beasts");
+                System.out.println("They select you, " + game.getPlayerName() + " to be that courageous soul.");
+                System.out.println("So, do you have what it takes to liberate your land with nothing but your sword, shield, and wit?");
+                System.out.println("Or will you succumb to defeat?");
+                System.out.println("Time to find out.");
+            } else {
+                System.out.println("Evil, menacing beast warriors from the Light Realm have been eradicating your mother land.");
+                System.out.println("They called you a barbarian for being of the Dark, and slaughtered your family and loved ones when you were young.");
+                System.out.println("It's time for revenge. You yield the spear of Moon and equip your shield, ready to take action.");
+                System.out.println("So, " + game.getPlayerName() + " will today be the day you raise your spear in victory over the vile dead bodies of your enemies?");
+                System.out.println("Or, will you succumb to their treacherous torture and die shamefully?");
+                System.out.println("Time to find out.");
+            }
             System.out.println("Beasts roam your hometown, terrorizing your family, friends, and people.");
             System.out.println("Your people select you as their hero, to help save this town.");
             System.out.println("Will you liberate them with your sword or fall to defeat? Time to find out.");
+            int winCount = 0;
             for (int i = 1; i <= game.getRound(); i++) {
-                System.out.println("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ New Game ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~");
+                System.out.println("~*~*~*~*~*~*~*~*~*~*~*~*~*~ Round " + i + " *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~");
                 System.out.println("A monster approaches with a health of 100!");
                 int currentHealth = (int) game.getPlayerHealth();
                 int monsterHealth = game.getMonsterHealth();
@@ -49,7 +70,9 @@ public class Logic {
                         monsterHealth -= dmg;
                         System.out.println("Successfully attacked Monster with " + dmg + " damage! Monster is now at " + monsterHealth + " health!");
                     }
+                    System.out.println("-");
                     System.out.println("Monster makes a move!");
+                    System.out.println("-");
                     proceed = (int) ((Math.random() * 3) + 1);
                     if (proceed == 3) {
                         System.out.println("Monster missed!");
@@ -67,12 +90,28 @@ public class Logic {
                             System.out.println("You shielded yourself, so half health that was lost is now returned! You are now at " + currentHealth + " heath!");
                         }
                     }
+                    currentHealth = game.bigLandMineChance(currentHealth);
+                    monsterHealth = bigLandMineMonsterChance(monsterHealth);
+                    System.out.println("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~");
                     System.out.println("Your Current Health: " + currentHealth);
                     System.out.println("Monster's Current Health: " + monsterHealth);
+                    System.out.println("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~");
+                }
+                if (monsterHealth < currentHealth) {
+                    winCount++;
+                    System.out.println("GAME OVER! The monster was successfully slain. You win this round!");
+                } else if (currentHealth < monsterHealth) {
+                    System.out.println("GAME OVER! You died this round!");
+                } else {
+                    System.out.println("You both died! Nobody wins! (dang yall suck)");
                 }
             }
-            //custom mode is here
-        } else {
+            System.out.println("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~ Final Results ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~");
+            System.out.println("Out of " + game.getRound() + " rounds, you won " + winCount + " times!");
+            if (winCount == 0) {
+                System.out.println("Haha, skill issue, loser. I'm just joking! Better luck next time! :D");
+            }
+        } else { //custom mode is here
             ProjectGame game = new ProjectGame("", 0, 100);
             while (userConfirm.equals("no") || userConfirm.equals("No") || userConfirm.equals("No.") || userConfirm.equals("no.")) {
                 System.out.print("Please enter your name: ");
@@ -91,5 +130,14 @@ public class Logic {
             }
             System.out.println("Great! Starting game....");
         }
+    }
+
+    private int bigLandMineMonsterChance (int stat) {
+        int bigBombChance = (int) ((Math.random() * 10) + 1);
+        if (bigBombChance == 1) {
+            stat -= 20;
+            System.out.println("Look! The monster stepped on a HUGE LANDMINE! The monster faces 20 damage!");
+        }
+        return stat;
     }
 }
--- src/ProjectGame.java
+++ src/ProjectGame.java
@@ -3,11 +3,13 @@ public class ProjectGame {
     private int playerRound;
     private double playerHealth;
     private int monsterHealth;
+    private String element;
     public ProjectGame (String playerName, int playerRound, double playerHealth) {
         this.playerName = playerName;
         this.playerRound = playerRound;
         this.playerHealth = playerHealth;
         this.monsterHealth = 100;
+        this.element = "";
     }
 
     public ProjectGame (String playerName, int playerRound) {
@@ -15,6 +17,7 @@ public class ProjectGame {
         this.playerRound = playerRound;
         this.playerHealth = 0;
         this.monsterHealth = 100;
+        this.element = "";
     }
 
     public void setPlayerHealth (int health) {
@@ -37,6 +40,10 @@ public class ProjectGame {
         return "Player Name: " + playerName + "\nPlayer Health: " + playerHealth + "\nSelected Number of Rounds: " + playerRound;
     }
 
+    public String getPlayerName () {
+        return playerName;
+    }
+
     public int getRound () {
         return playerRound;
     }
@@ -45,8 +52,12 @@ public class ProjectGame {
         return playerHealth;
     }
 
-    public int getMonsterHealth() {
+    public int getMonsterHealth () {
         return monsterHealth;
+    }
+
+    public String getElement () {
+        return element;
     }
 
     public int playerAttack () {
@@ -57,5 +68,23 @@ public class ProjectGame {
     public int monsterAttack () {
         int attackAmount = (int) ((Math.random() * 10) + 1); // range from 1 to 10
         return attackAmount;
+    }
+
+    public int bigLandMineChance (int stat) {
+        int bigBombChance = (int) ((Math.random() * 10) + 1);
+        if (bigBombChance == 1) {
+            stat -= 20;
+            System.out.println("Uh oh! A HUGE LANDMINE WENT OFF!! You face 20 damage!");
+        }
+        return stat;
+    }
+
+    public void elementSelect (String str, int input) { // The string will be entered with 2 words, and a comma and space between those two words. ex: "Ice, Fire"
+        int commaIndex = str.indexOf(",");
+        if (input == 1) {
+            element = str.substring(0, commaIndex);
+        } else {
+            element = str.substring(commaIndex+2);
+        }
     }
 }
